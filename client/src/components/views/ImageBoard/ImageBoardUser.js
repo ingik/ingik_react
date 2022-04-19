6266ed4420174c0aa403a0113cda0c2a6cd9039d
ingik <skskoe@naver.com>
2022-04-20 03:59:19 +0900

    qa_

--- client/src/components/views/ImageBoard/ImageBoardUser.js
+++ client/src/components/views/ImageBoard/ImageBoardUser.js
@@ -1,13 +1,30 @@
-import { Avatar, Popover, Typography } from '@mui/material'
+import { Avatar, Button, Popover, Typography } from '@mui/material'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import axios from 'axios'
 import React, { useState,useEffect } from 'react'
 import {  useHistory } from 'react-router-dom'
 import HoverProfile from '../Profile/HoverProfile'
 
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useSelector } from 'react-redux'
+
 function ImageBoardUser(props) {
+
+  const UserSelectData = useSelector((state) => state.user.userData);
+
 
   const history = useHistory();
   const [UserData, setUserData] = useState({});
+
+  const [DialogOpen, setDialogOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
 
     useEffect(() => {
       let ComponentMounted = true;
@@ -51,6 +68,11 @@ function ImageBoardUser(props) {
 
   const getFunc = (data) => {
     setAnchorEl(data)
+  }
+
+  const onDeleteButton = () => {
+    setDialogOpen(true);
+
   }
 
 
@@ -116,6 +138,38 @@ function ImageBoardUser(props) {
       >
         {UserData?.name}
       </Typography>
+      {
+        props.userId === UserSelectData._id ?
+        <Button 
+          sx={{float:'right'}}
+          onClick={onDeleteButton}
+        >
+          <DeleteForeverIcon />
+        </Button>
+        : null
+      }
+
+      <Dialog
+        open={DialogOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"이 게시물을 삭제하시겠습니까?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            한번 삭제한 게시물은 복구 시킬 수 없습니다.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant>삭제</Button>
+          <Button onClick={handleClose} autoFocus>
+            닫기
+          </Button>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   );
 }
