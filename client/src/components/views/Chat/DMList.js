95eea64d10fe2a2c321c3a08fb0331ad54a598fe
ingik <skskoe@naver.com>
2022-04-25 14:33:48 +0900

    ec2 socket

--- client/src/components/views/Chat/DMList.js
+++ client/src/components/views/Chat/DMList.js
@@ -8,12 +8,12 @@ import './Chat.css';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 
-// const localhost =
-//   process.env.NODE_ENV 
-//     ? 'http://3.36.133.116:5555'
-//     : 'http://localhost:5555';
+const localhost =
+  process.env.NODE_ENV 
+    ? 'http://3.36.133.116:5555'
+    : 'http://localhost:5555';
 
-const localhost = "http://localhost:5000"
+// const localhost = "http://localhost:5555"
 
 let socket
 
