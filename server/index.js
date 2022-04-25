1ed16c30c35b520f27653b051e018511e31faed8
ingik <skskoe@naver.com>
2022-04-25 12:53:02 +0900

    ec2

--- server/index.js
+++ server/index.js
@@ -792,10 +792,15 @@ app.post('/api/DirectMessage/input/',(req,res) => {
 const Server = require('socket.io');
 const { time } = require('console');
 
+const URL =
+  process.env.NODE_ENV 
+    ? 'http://3.36.133.116:5000'
+    : 'http://localhost:3000';
+
 const server = require('http').Server(app);
 const io = Server(server,{
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [URL],
     methods:["GET","POST"],
 }
 });
