19f10c75e26ecd06ab325e23bb41803d673c260e
ingik <skskoe@naver.com>
2022-04-29 03:19:45 +0900

    pm2

--- server/index.js
+++ server/index.js
@@ -31,7 +31,9 @@ const imageDelete = require('./S3/imageDelete')
 app.use(express.urlencoded({extended: true}));
 
 //application/json
-app.use(express.json());
+app.use(express.json({
+  limit : "50mb"
+}));
 app.use(cookieParser());
 
 
